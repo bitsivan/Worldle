6ab00a2da941b9f270308207e2419952c2d98e91
ivan montejo <sergio.montejo@improving.com>
2023-07-14 13:22:32 -0600

    Queue list updated

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net.Mime;
+using System.Linq;
+// See https://aka.ms/new-console-template for more information
 //Sergio Ivan Motejo Olan           sergio.montejo@improving.com          07/14/2023
 Console.WriteLine("Hello, World!");
 
@@ -7,16 +9,58 @@ game.WordleGame();
 
 class Game
 {
+    string wordToGuess = "build";
+
     public void WordleGame()
     {
-        string wordToGuess = "build";
-        string input = null;
-        input = Console.ReadLine();
+        string option = null;
+
+        Console.WriteLine("Single word (type 1) o queue 5 words to try (type 2)");
+        option = Console.ReadLine();
+
+        if (option == "1")
+        {
+            string input = null;
+            Console.WriteLine("Write the word");
+            Console.WriteLine();
+            input = Console.ReadLine();
+            this.executeGuessWord(input);
+        }
+        else if (option == "2")
+        {
+            IList<string> listWords = new List<string>();
+            string wordQ = "";
+            Console.WriteLine("Write the 5 words:");
+            for (int i = 0; i < 6; i++)
+            {
+                wordQ = Console.ReadLine();
+                listWords.Add(wordQ);
+            }
+            Console.WriteLine("...checking list of words");
+
+            foreach (var item in listWords)
+            {
+                executeGuessWord(item);
+            }
+
+            Environment.Exit(0);
+        }
+
+
+
+
+
+    }
+
+    public void executeGuessWord(string input)
+    {
+
         if (input.Length == 5)
         {
             if (input == wordToGuess)
             {
                 Console.WriteLine("Awesome, you guessed the word --> " + wordToGuess);
+                Environment.Exit(0);
             }
             else
             {
@@ -39,15 +83,19 @@ class Game
                     }
                 }
             }
-            Console.ResetColor();
-            WordleGame();
+            Console.ResetColor(); 
+            Console.WriteLine();
+            input = Console.ReadLine();
+            executeGuessWord(input);
         }
         else
         {
-            Console.BackgroundColor= ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("The game require a word with 5 letters");
             Console.ResetColor();
-            WordleGame();
+            Console.WriteLine(); 
+            input = Console.ReadLine();
+            executeGuessWord(input);
         }
     }
 }
